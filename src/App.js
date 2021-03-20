29c80799902906fbc6d483677b871c96e4b3b50b
M sabid <sabidsabid77@gmail.com>
2021-03-20 21:07:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [post, setPost] = useState([]);
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+      .then(res => res.json())
+      .then(data => setPost(data))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Here Is {post.length} Post.
+      </h1>
+      <div className='body-section'>
+        <div className='post-section'>
+          {
+            post.map(post => (
+              <div className="post-container">
+                <h3 id='post-text'>{post.title}</h3>
+                <p id='post-text'>
+                  {post.body}
+                </p>
+                <button id='more-btn' onClick={() => {
+                  setCount(count + 1)
+                }}>Know More</button>
+              </div>
+            ))
+          }
+        </div>
+        <div className='cart'>
+          <h3>Added Post: {count}</h3>
+        </div>
+      </div>
     </div>
   );
 }
